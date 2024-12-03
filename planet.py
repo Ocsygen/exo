58bf80e9b436d884542799e6c9a6894015fa0676
Ocsygen <98640197+Ocsygen@users.noreply.github.com>
2024-12-03 16:49:10 +0400

    Update planet.py

--- planet.py
+++ planet.py
@@ -1,34 +1,64 @@
 import csv
 import mysql.connector
-from tabulate import *
+from tabulate import tabulate
+
+conobj = mysql.connector.connect(host='localhost', user='root', passwd='groot', database='stars') 
+curry = conobj.cursor() 
+headers = [] 
+
+def isempty_table():
+    global curry
+    count_query = "SELECT COUNT(*) FROM planets"
+    curry.execute(count_query)
+    count = curry.fetchone()[0]
+    return count == 0
+
 def create_table():
-    with open('planets.csv','r',encoding='utf8') as f:
-        reader=csv.reader(f)
-        heading=next(reader)
-        data=list(reader)
-        columns={}
+    global conobj, curry, headers
+    with open('planets.csv', 'r', encoding='utf8') as f:
+        reader = csv.reader(f)
+        heading = next(reader)
+        data = list(reader)
+        columns = {}
+
         for i in data:
-            for index,value in enumerate(i):
-                dataheader=heading[index]
+            for index, value in enumerate(i):
+                dataheader = heading[index]
                 if value.isdigit():
-                    datatype='INT'
+                    datatype = 'INT'
                     try:
                         float(value)
-                        datatype ='FLOAT'
+                        datatype = 'FLOAT'
                     except:
                         pass
                 else:
-                    datatype='VARCHAR(255)'
-                    columns[dataheader]=datatype
-            query1='CREATE TABLE %s IF NOT EXIST('%['Planets']
-            query2=','.join([f"{dataheader} {datatype}" for dataheader,datatype in columns.items()])
-            query3=')'
-            query = query1 + query2 + query3
-            conobj=mysql.connector(host='localhost',user='root',passswd='groot',database='stars')
-            curry=conobj.cursor()
-            curry.execute(query)
-            curry.execute('desc planets')
-            re=curry.fetchall()
-            headers=[desc[0] for desc in curry.description]
-            print(tabulate(re,headers,tablefmt='grid'))
+                    datatype = 'VARCHAR(255)'
+                if dataheader not in columns:
+                    columns[dataheader] = datatype
+
+        query1 = 'CREATE TABLE IF NOT EXISTS Planets ('
+        query2 = ', '.join([f"{dataheader} {datatype}" for dataheader, datatype in columns.items()])
+        query3 = ')'
+        query = query1 + query2 + query3
+
+        curry.execute(query)
+        curry.execute('DESC Planets')
+        re = curry.fetchall()
+        headers = [desc[0] for desc in curry.description]
+        print(tabulate(re, headers, tablefmt='grid'))
+
+def view_table():
+    global curry, headers
+    view_query = "SELECT * FROM planets"
+    curry.execute(view_query)
+    view_forloopvar = curry.fetchall()
+    headers = [desc[0] for desc in curry.description]
+    if is_table_empty(): 
+        print("The Table is Empty!") 
+    else: 
+        print(tabulate(view_forloopvar, headers, tablefmt='grid'))
+
 create_table()
+view_table()
+curry.close
+conobj.close
